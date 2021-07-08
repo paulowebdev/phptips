452e43200dea7abe4f27b53d6ec6e4985917494b
Paulo Marcelo Gonçalves <p.marcelo92@gmail.com>
2021-07-08 09:40:51 -0300

    Add files via upload

---
+++ ep01/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => 'paulowebdev/phptips',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'coffeecode/datalayer' => array(
+            'pretty_version' => '1.1.8',
+            'version' => '1.1.8.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../coffeecode/datalayer',
+            'aliases' => array(),
+            'reference' => '52e209190752eccc0bfd8ff3abcdd607ed4c01c1',
+            'dev_requirement' => false,
+        ),
+        'paulowebdev/phptips' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+    ),
+);
